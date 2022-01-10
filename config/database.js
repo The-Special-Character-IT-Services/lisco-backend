ba6839edd9efaf0fcb3bc1fb73ed6f9a4fcf6715
Yagnesh Modh <hey@yagneshmodh.com>
2022-01-10 15:09:50 +0530

    commit code

--- config/database.js
+++ config/database.js
@@ -2,12 +2,14 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
+      host: env('DATABASE_HOST', 'ec2-52-54-237-144.compute-1.amazonaws.com'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'lisco_backend'),
-      user: env('DATABASE_USERNAME', ''),
-      password: env('DATABASE_PASSWORD', ''),
-      ssl: env.bool('DATABASE_SSL', false),
+      database: env('DATABASE_NAME', 'd5ohgoo4bdcepo'),
+      user: env('DATABASE_USERNAME', 'qxqdxhlxeyzuwf'),
+      password: env('DATABASE_PASSWORD', '19802f55412fe66b106e1fdde673c48016ca7dd61a675a91ed4a0264f9800b99'),
+      ssl: {
+        rejectUnauthorized: false
+      },
     },
   },
 });
