c8a355d0f145cfa7287f4a8bd8cb51c4566f007e
Yagnesh Modh <hey@yagneshmodh.com>
2022-02-01 17:54:36 +0530

    social media commit

--- config/server.js
+++ config/server.js
@@ -1,4 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  app: {
+    keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),
+  },
+  url: 'https://6c45-27-57-188-217.ngrok.io'
 });
